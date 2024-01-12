6e5628100f151c9d33a18f01a3f6651bc356a615
Vivek <vivekyadav7272@gmail.com>
2024-01-12 16:26:41 +0530

    made the console.error hook consistent across different parts of extension

--- src/extension_ui/homepage.rs
+++ src/extension_ui/homepage.rs
@@ -3,7 +3,7 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn start_app() {
-    std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+    console_error_panic_hook::set_once();
     dioxus_web::launch(app);
 }
 
