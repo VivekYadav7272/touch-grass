c1493352f987f81067493ef166e6991092272ac0
Vivek <vivekyadav7272@gmail.com>
2024-01-12 23:27:18 +0530

    fixed a stupid little typo bug

--- extension/index.js
+++ extension/index.js
@@ -1,7 +1,7 @@
 console.log("Hello from extension/index.js");
 
 (async () => {
-    const myModule = await import(browser.runtime.getURL("./.touch_grass.js"));
+    const myModule = await import(browser.runtime.getURL("./touch_grass.js"));
     await myModule.default();
     setTimeout(() => myModule.touch_grass(), 2000);
 })();
