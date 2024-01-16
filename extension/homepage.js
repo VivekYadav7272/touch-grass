304c72476cf0cf8a1f7714a95c10e5537e6dfa18
Jonas <92295347+jpx40@users.noreply.github.com>
2024-01-16 17:38:22 +0100

    update

--- extension/homepage.js
+++
@@ -1,8 +0,0 @@
-console.log("Hello from extension/homepage.js");
-
-import * as myModule from "./touch_grass.js";
-
-(async () => {
-    await myModule.default("./touch_grass_bg.wasm");
-    myModule.start_app();
-})();
