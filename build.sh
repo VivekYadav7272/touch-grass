e9afb69cb028099c795c8088f6e88b1cc989122d
Vivek <vivekyadav7272@gmail.com>
2024-07-14 14:43:08 +0530

    added command to generate output.css from tailwind

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 wasm-pack build --target=web
+npx tailwindcss -i ./src/extension_ui/input.css -o ./extension/output.css
+
 rm -r ./extension/snippets/
 rm ./extension/touch_grass_bg.wasm
 rm ./extension/touch_grass.js
+rm ./extension/output.css
+
 cp -r ./pkg/snippets ./extension/snippets
 cp ./pkg/touch_grass_bg.wasm ./extension/touch_grass_bg.wasm
 cp ./pkg/touch_grass.js ./extension/touch_grass.js
