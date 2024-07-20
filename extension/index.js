03f780a7612640a525954062416a92b802214aea
Vivek <vivekyadav7272@gmail.com>
2024-07-20 16:03:46 +0530

    added some helpful comment to understand discrepancy in code style

--- extension/index.js
+++ extension/index.js
@@ -1,5 +1,7 @@
 console.log("Hello from extension/index.js");
 
+// Can't do the `import * as ...` thing cuz I'm not a top-level module here, I'm just a poor
+// little injected script.
 (async () => {
     const myModule = await import(browser.runtime.getURL("./touch_grass.js"));
     await myModule.default();
