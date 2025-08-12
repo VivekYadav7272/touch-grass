f43b01a3b17e5e94d232422dc18ae2aa39fd5f1c
Vivek <vivekyadav7272@gmail.com>
2025-08-13 00:17:02 +0530

    update build commands and lockfiles

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 wasm-pack build --target=web
 
-rm -r ./extension/snippets/
-rm ./extension/touch_grass_bg.wasm
-rm ./extension/touch_grass.js
-rm ./extension/output.css
+rm -r ./extension/snippets/ || true
+rm ./extension/touch_grass_bg.wasm || true
+rm ./extension/touch_grass.js || true
+rm ./extension/output.css || true
 
-npx tailwindcss -i ./src/extension_ui/input.css -o ./extension/output.css
+npm install -D tailwindcss
+
+npx tailwindcss@3 -i ./src/extension_ui/input.css -o ./extension/output.css
 cp -r ./pkg/snippets ./extension/snippets
 cp ./pkg/touch_grass_bg.wasm ./extension/touch_grass_bg.wasm
 cp ./pkg/touch_grass.js ./extension/touch_grass.js
